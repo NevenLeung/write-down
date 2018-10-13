53089aa89d2c43949f988aa545319143e414374e
Neven Leung <nevyn.leong@gmail.com>
2018-10-14 05:06:39 +0800

    [Added] some plugins to markdown parser.

--- src/components/MarkdownParser.js
+++ src/components/MarkdownParser.js
@@ -1,17 +1,26 @@
 import MarkdownIt from "markdown-it";
 
+import abbr from 'markdown-it-abbr';
 import anchor from "markdown-it-anchor";
-import TOC from "markdown-it-table-of-contents";
-import taskList from "markdown-it-task-lists";
+import deflist from 'markdown-it-deflist';
 import emoji from "markdown-it-emoji";
+import footnote from 'markdown-it-footnote';
 import highlight from "markdown-it-highlightjs";
+import imsize from 'markdown-it-imsize';
+import mark from 'markdown-it-mark';
+import sub from 'markdown-it-sub';
+import sup from 'markdown-it-sup';
+import taskList from "markdown-it-task-lists";
+import TOC from "markdown-it-table-of-contents";
+
+import twemoji from 'twemoji';
 
 const markdownOption = {
   html:         false,        // Enable HTML tags in source
   xhtmlOut:     false,        // Use '/' to close single tags (<br />)
   breaks:       false,        // Convert '\n' in paragraphs into <br>
   langPrefix:   'language-',  // CSS language prefix for fenced blocks
-  linkify:      true,         // autoconvert URL-like texts to links
+  linkify:      false,         // autoconvert URL-like texts to links
   typographer:  true,         // Enable smartypants and other sweet transforms
   quotes: '“”‘’',
 };
@@ -23,20 +32,36 @@ const anchorOption = {
   level: [1, 2, 3, 4, 5, 6]
 };
 
-const TOC_Option = {
-  includeLevel: [1, 2, 3, 4, 5, 6]
+const imsizeOption = {
+  autofill: true
 };
-
 
 const taskListOption = {
   enabled: true,
   label: true
 };
 
-md.use(anchor, anchorOption)
-  .use(TOC, TOC_Option)
-  .use(taskList)
+const TOC_Option = {
+  includeLevel: [1, 2, 3, 4, 5, 6]
+};
+
+
+md.use(abbr)
+  .use(anchor, anchorOption)
+  .use(deflist)
   .use(emoji)
-  .use(highlight);
+  .use(footnote)
+  .use(highlight)
+  .use(imsize, imsizeOption)
+  .use(mark)
+  .use(sub)
+  .use(sup)
+  .use(taskList)
+  .use(TOC, TOC_Option);
+
+// change the output of emoji
+md.renderer.rules.emoji = function(token, idx) {
+  return twemoji.parse(token[idx].content);
+};
 
 export default md;
