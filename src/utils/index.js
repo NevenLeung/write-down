d22e811eb0319838e0f26c3ff18cd39aa1c45bac
Neven Leung <nevyn.leong@gmail.com>
2018-11-16 23:59:37 +0800

    [Updated] the code of utils.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,35 @@
+/**
+ * delay 生成一个异步的延时
+ *
+ * @param timeout
+ * @return {Promise<any>}
+ */
 function delay(timeout) {
   return new Promise(resolve => setTimeout(resolve, timeout));
 }
 
+/**
+ * checkImageUrlIsValid 检查url是否是合法的
+ *
+ * @param url
+ * @return {Promise<any>}
+ */
+function checkImageUrlIsValid(url) {
+  return new Promise(function(resolve, reject) {
+    const img = new Image();
+    img.onload = function() {
+      resolve(url);
+    };
+
+    img.onerror = function(){
+      reject('The url of image is invalid.');
+    };
+
+    img.src = url;
+  });
+}
+
 export {
-  delay
+  delay,
+  checkImageUrlIsValid
 }
