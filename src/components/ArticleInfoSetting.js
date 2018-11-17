ff1b43cc7affa4fe57d1ff13feea4f48afda3a36
Neven Leung <nevyn.leong@gmail.com>
2018-11-17 08:46:51 +0800

    [Added] the code of showing article info setting popover preview when hovering on the info setting button.

--- src/components/ArticleInfoSetting.js
+++ src/components/ArticleInfoSetting.js
@@ -1,20 +1,69 @@
 import React, { Component } from "react";
-import { Form, Icon, Input, Popover, Modal, Collapse ,Tabs, Button } from "antd";
+import { Form, Icon, Input, Popover, Modal, Collapse ,Tabs, Button, Row, Col } from "antd";
 
 import PhotoSearch from "./PhotoSearch";
 import { checkImageUrlIsValid } from "../utils";
-import formData from './data';
+import mockData from './data';
 
-import styles from "./Header.module.css";
+import styles from "./ArticleInfoSetting.module.css";
+
+const metaData = mockData[1];
+
+class InfoSettingPreview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coverUrl: '',
+      isCoverUrlValid: false
+    };
+  }
+
+  async componentDidMount() {
+    const coverUrl = await checkImageUrlIsValid(metaData.coverUrl);
+
+    if (coverUrl) {
+      this.setState({
+        coverUrl: coverUrl,
+        isCoverUrlValid: true
+      });
+    } else {
+      this.setState({
+        coverUrl: coverUrl,
+        isCoverUrlValid: false
+      });
+    }
+  }
+
+
+  render() {
+    return (
+      <div className={styles.infoPreviewWrapper}>
+        {
+          this.state.isCoverUrlValid
+            ? <div className={styles.infoPreviewImageWrapper}>
+                <div
+                  className={styles.infoPreviewImage}
+                  style={{backgroundImage: `url(${this.state.coverUrl})`}}
+                />
+              </div>
+            : null
+        }
+        <div>
+          <h2 className={styles.infoPreviewTitle}>{metaData.title}</h2>
+          <p className={styles.infoPreviewExcerpt}>{metaData.excerpt}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 const InfoSettingButton = props => {
   return (
     <Popover
-      content={
-        <div>content</div>
-      }
+      content={<InfoSettingPreview/>}
       title="Article Info Preview"
       trigger={['hover']}
+      mouseEnterDelay={0.5}
     >
       <button className={styles.button} onClick={props.onClick}>
         <Icon type="profile" theme="outlined" />
@@ -24,8 +73,8 @@ const InfoSettingButton = props => {
 };
 
 const formItemLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 20 },
+  labelCol: { span: 6 },
+  wrapperCol: { span: 14 },
 };
 
 class ArticleInfoSettingModal extends React.Component {
@@ -74,7 +123,7 @@ class ArticleInfoSettingModal extends React.Component {
         <InfoSettingButton onClick={this.showModal}/>
         <Modal
           style={{top: 20}}
-          width={920}
+          width={880}
           title="Article Info Setting"
           visible={this.state.visible}
           onCancel={this.handleCancel}
@@ -102,7 +151,7 @@ class ArticleInfoForm extends Component {
 
   componentDidMount() {
     // To load the cover based on saved url.
-    this.checkCoverUrl(undefined, formData.coverUrl, ()=>{});
+    this.checkCoverUrl(undefined, metaData.coverUrl, ()=>{});
     // To disabled submit button at the beginning.
     this.props.form.validateFields();
   }
@@ -144,7 +193,7 @@ class ArticleInfoForm extends Component {
     // 这里只需要使用data提交表单的数据即可
     console.log(data);
     this.props.afterSubmit();
-    // console.log(formData);
+    // console.log(metaData);
   };
 
   render() {
@@ -155,7 +204,7 @@ class ArticleInfoForm extends Component {
 
     return (
       <div>
-        <Form layout={'vertical'}>
+        <Form>
           <Form.Item
             {...formItemLayout}
             label={'Title'}
@@ -163,7 +212,7 @@ class ArticleInfoForm extends Component {
             // help={titleError || ''}
           >
             {getFieldDecorator('title', {
-              initialValue: formData.title,
+              initialValue: metaData.title,
               rules: [
                 {
                   required: true,
@@ -184,7 +233,7 @@ class ArticleInfoForm extends Component {
             // validateStatus={excerptError ? 'error' : ''}
           >
             {getFieldDecorator('excerpt', {
-              initialValue: formData.excerpt,
+              initialValue: metaData.excerpt,
               rules: [
                 {
                   required: true,
@@ -205,76 +254,81 @@ class ArticleInfoForm extends Component {
           </Form.Item>
           <Form.Item
             {...formItemLayout}
-            label={'Cover'}
+            label={'Cover Url'}
           >
             {getFieldDecorator('cover', {
-              initialValue: formData.coverUrl,
+              initialValue: metaData.coverUrl,
               rules: [{
-                required: true,
                 validator: this.checkCoverUrl
               }]
             })(
               <Input placeholder="Please input a valid url of the cover of your article. " />
             )}
           </Form.Item>
-          <div className={styles.coverPreviewArea}>
-            {
-              this.state.isCoverUrlValid
-                ? <img className={styles.coverPreviewImage} src={this.state.coverUrl} alt=" loading..."/>
-                : (
-                  <div className={styles.coverPreviewAreaTipWrapper}>
-                    {
-                      this.state.isUrlChanged
-                        ? <p className={styles.coverPreviewAreaTipWithFeedback}>{this.state.coverPreviewMsg}</p>
-                        : <p className={styles.coverPreviewAreaTip}>{this.state.coverPreviewMsg}</p>
-                    }
-                  </div>
-                )
-            }
-          </div>
-          <Form.Item style={{margin: 0}}>
-            <Collapse bordered={false}>
-              <Collapse.Panel header={"Tips for setting cover"} key={"tips"}>
-                <pre className={styles.coverSettingTip}>
-                Choose an approach to set your cover:
-                <br/>
-                1. Search photo from Unsplash by keyword and click on the photo you selected. The url of the photo will be filled in automatically.
-                <br/>
-                2. Upload the cover to image hosting service, and paste the valid image url to the cover field.
-                <br/>
-                <br/>
-                Important: Image in landscape mode is recommended.
-                </pre>
-              </Collapse.Panel>
-            </Collapse>
-
-            <Tabs defaultActiveKey="1">
-              <Tabs.TabPane tab="Choose a photo from Unsplash" key="1">
-                <PhotoSearch selectCover={this.selectCover}/>
-              </Tabs.TabPane>
-              <Tabs.TabPane tab="Upload your photo" key="2">
-                <div style={{marginTop: 12}}>
-                  <pre className={styles.coverSettingTip}>
-                    You can use any image hosting service to store your cover, like <a href="http://imgur.com/" target="_blank" rel="noopener noreferrer">Imgur</a>, <a href="https://www.dropbox.com/" target="_blank" rel="noopener noreferrer">Dropbox</a>, <a href="https://imageshack.us/" target="_blank" rel="noopener noreferrer">Imageshack</a>, and other free image hosting service.
-                    <br/>
-                    <br/>
-                    Remember to paste url back to the cover field.
-                  </pre>
-                </div>
-              </Tabs.TabPane>
-            </Tabs>
-          </Form.Item>
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={hasErrors(getFieldsError())}
-              onClick={this.onSubmit}
-            >
-              Submit
-            </Button>
-          </Form.Item>
+          <Row>
+            <Col span={6} className={styles.submitButtonWrapper}>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  disabled={hasErrors(getFieldsError())}
+                  onClick={this.onSubmit}
+                >
+                  Submit
+                </Button>
+              </Form.Item>
+            </Col>
+            <Col span={14}>
+              <div className={styles.coverPreviewArea}>
+                {
+                  this.state.isCoverUrlValid
+                    ? <img className={styles.coverPreviewImage} src={this.state.coverUrl} alt=" loading..."/>
+                    : (
+                      <div className={styles.coverPreviewAreaTipWrapper}>
+                        {
+                          this.state.isUrlChanged
+                            ? <p className={styles.coverPreviewAreaTipWithFeedback}>{this.state.coverPreviewMsg}</p>
+                            : <p className={styles.coverPreviewAreaTip}>{this.state.coverPreviewMsg}</p>
+                        }
+                      </div>
+                    )
+                }
+              </div>
+            </Col>
+          </Row>
         </Form>
+        <div>
+          <Collapse bordered={false}>
+            <Collapse.Panel header={"Tips for setting cover"} key={"tips"}>
+              <pre className={styles.coverSettingTip}>
+              Choose an approach to set your cover:
+              <br/>
+              1. Search photo from Unsplash by keyword and click on the photo you selected. The url of the photo will be filled in automatically.
+              <br/>
+              2. Upload the cover to image hosting service, and paste the valid image url to the cover field.
+              <br/>
+              <br/>
+              Important: Image in landscape mode is recommended.
+              </pre>
+            </Collapse.Panel>
+          </Collapse>
+
+          <Tabs defaultActiveKey="1">
+            <Tabs.TabPane tab="Choose a photo from Unsplash" key="1">
+              <PhotoSearch selectCover={this.selectCover}/>
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Upload your photo" key="2">
+              <div style={{marginTop: 12}}>
+                <pre className={styles.coverSettingTip}>
+                  You can use any image hosting service to store your cover, like <a href="http://imgur.com/" target="_blank" rel="noopener noreferrer">Imgur</a>, <a href="https://www.dropbox.com/" target="_blank" rel="noopener noreferrer">Dropbox</a>, <a href="https://imageshack.us/" target="_blank" rel="noopener noreferrer">Imageshack</a>, and other free image hosting service.
+                  <br/>
+                  <br/>
+                  Remember to paste url back to the cover field.
+                </pre>
+              </div>
+            </Tabs.TabPane>
+          </Tabs>
+        </div>
       </div>
     );
   }
--- src/components/data.js
+++ src/components/data.js
@@ -1,11 +1,45 @@
-let formData = {
-  _id: '1',
-  title: 'Title',
-  excerpt: 'This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt. This is the excerpt.',
-  coverUrl: 'https://images.unsplash.com/photo-1542173212-6fbb91e107f1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e0f80857a9986c67b43b0ce666adf327&auto=format&fit=crop&w=376&q=80',
-  createdAt: '2018-11-15',
-  updatedAt: '2018-11-16',
-  tags: ['story', 'daily']
-};
+let formData = [
+  {
+    _id: 1,
+    title: 'Title',
+    author: 'Author name',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit massa enim nec dui. At tellus at urna condimentum mattis pellentesque. Volutpat est velit egestas dui id ornare arcu odio ut. Purus viverra accumsan in nisl nisi.',
+    coverUrl: 'https://images.unsplash.com/photo-1503363876019-10eaf537e61d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjQxMzU5fQ&s=a69ae344deb3588f0caebc5cbd2bcac7',
+    createdAt: '2018-11-15',
+    updatedAt: '2018-11-16',
+    tags: ['story', 'daily']
+  },
+  {
+    _id: 2,
+    title: 'Title',
+    author: 'Author name',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit massa enim nec dui. At tellus at urna condimentum mattis pellentesque. Volutpat est velit egestas dui id ornare arcu odio ut. Purus viverra accumsan in nisl nisi.',
+    coverUrl: 'https://images.unsplash.com/photo-1464655646192-3cb2ace7a67e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjQxMzU5fQ&s=e5c92940908160ed6b1fce199d70df14',
+    createdAt: '2018-11-15',
+    updatedAt: '2018-11-16',
+    tags: ['story', 'daily']
+  },
+  {
+    _id: 3,
+    title: 'Title',
+    author: 'Author name',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit massa enim nec dui. At tellus at urna condimentum mattis pellentesque. Volutpat est velit egestas dui id ornare arcu odio ut. Purus viverra accumsan in nisl nisi.',
+    coverUrl: '',
+    createdAt: '2018-11-15',
+    updatedAt: '2018-11-16',
+    tags: ['story', 'daily']
+  },
+  {
+    _id: 4,
+    title: 'Title',
+    author: 'Author name',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus molestie nunc non blandit massa enim nec dui. At tellus at urna condimentum mattis pellentesque. Volutpat est velit egestas dui id ornare arcu odio ut. Purus viverra accumsan in nisl nisi.',
+    coverUrl: 'https://images.unsplash.com/photo-1542173212-6fbb91e107f1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e0f80857a9986c67b43b0ce666adf327&auto=format&fit=crop&w=1080&q=80',
+    createdAt: '2018-11-15',
+    updatedAt: '2018-11-16',
+    tags: ['story', 'daily']
+  }
+
+];
 
 export default formData;
