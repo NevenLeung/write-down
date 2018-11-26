c5f2f97fec3efcf59ad3c8ea231478cdcb049e43
Neven Leung <nevyn.leong@gmail.com>
2018-11-26 23:13:25 +0800

    Updates

---
+++ precache-manifest.08eb8ce3be5bd9413798d3a6e72a9e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e0de25ca446b9f3872c",
+    "url": "/write-down/static/js/runtime~main.5e0de25c.js"
+  },
+  {
+    "revision": "21985b4f8a4f774d246e",
+    "url": "/write-down/static/js/main.21985b4f.chunk.js"
+  },
+  {
+    "revision": "68a846b2aea9e58d6769",
+    "url": "/write-down/static/js/1.68a846b2.chunk.js"
+  },
+  {
+    "revision": "21985b4f8a4f774d246e",
+    "url": "/write-down/static/css/main.9d051577.chunk.css"
+  },
+  {
+    "revision": "68a846b2aea9e58d6769",
+    "url": "/write-down/static/css/1.1452e422.chunk.css"
+  },
+  {
+    "revision": "9786dbffa60c4ac5fb21f82cc3d37cab",
+    "url": "/write-down/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/write-down/precache-manifest.19725931448a7be02c6bb46c57976f91.js"
+  "/write-down/precache-manifest.08eb8ce3be5bd9413798d3a6e72a9e08.js"
 );
 
 workbox.clientsClaim();
