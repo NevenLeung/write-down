ff4ba34126bbd20253b2c68e49f62f1a1251a901
Neven Leung <nevyn.leong@gmail.com>
2019-01-12 23:59:25 +0800

    [Added] the basic article reading component.

---
+++ src/components/article-read/ArticleRead.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Row, Col } from 'antd';
+
+import styles from './ArticleRead.modules.css';
+
+const ArticleRead = (props) => {
+
+
+  return (
+    <Row type="flex" justify="center" align="middle">
+      <Col className={styles.container}>
+
+      </Col>
+    </Row>
+  )
+};
+
+const Title = () => (
+  <div>
+    Title
+  </div>
+);
+
+const HeaderPhoto = () => (
+  <div>
+    Header Photo
+  </div>
+);
+
+const Excerpt = () => (
+  <div>
+    Excerpt
+  </div>
+);
+
+const content = () => (
+  <div>
+    content
+  </div>
+);
+
