e21251c36baf5417cee31c27280fbd56225fe87e
Neven Leung <nevyn.leong@gmail.com>
2019-01-17 20:28:14 +0800

    [Updated] the code of article and draft list by adding a back to top button. [Updated] the style of global imported font types and info setting preview title.

--- src/components/articles/ArticleDraftList.js
+++ src/components/articles/ArticleDraftList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Col, Row, Divider } from "antd";
+import { Col, Row, Divider, BackTop } from "antd";
 
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime'
@@ -53,6 +53,7 @@ class ArticleDraftListPage extends Component {
           <Col className={styles.list} md={16} sm={20} xs={24}>
             <Divider className={styles.pageIndicator}>Draft list</Divider>
             {DraftList? DraftList: null}
+            <BackTop/>
           </Col>
           <Col md={4} sm={2} xs={0}>
 
