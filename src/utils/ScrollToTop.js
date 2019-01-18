fb4fb5ca8d49f5463d7a09e46c7b3d598ce30c8f
Neven Leung <nevyn.leong@gmail.com>
2019-01-18 23:56:28 +0800

    [Added] a component of scrolling to page top to fixed the issue which react-router doesn't reset the scroll after route transition.

---
+++ src/utils/ScrollToTop.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ScrollToTop extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default ScrollToTop;
