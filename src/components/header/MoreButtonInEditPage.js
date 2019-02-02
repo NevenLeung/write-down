2d3f891d7bd8d4c3de498e6501af54e6a82674c6
Neven Leung <nevyn.leong@gmail.com>
2019-02-03 02:25:06 +0800

    [Refactor] the code of more button component in editing page.

--- src/components/header/MoreButtonInEditPage.js
+++ src/components/header/MoreButtonInEditPage.js
@@ -1,49 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import { Dropdown, Icon, Menu } from "antd";
 
 import { exportFile, generateStyledHTML } from "../../utils";
 
 import styles from "./Header.module.css";
 
-class MoreMenuInEditPage extends Component {
-  handleMenuItemClick= ({item, key, keyPath}) => {
-    if (key === 'markdown') {
-      exportFile(this.props.markdown, 'text/markdown; charset=utf-8', this.props.title + '.md');
-    } else if (key === 'html') {
-      const file = generateStyledHTML(this.props.title, this.props.html);
+const MoreMenuInEditPage = ({ data, ...rest }) => {
+  const handleMenuItemClick= ({item, key, keyPath}) => {
+    const { title, markdown } = data;
 
-      exportFile(file, 'text/plain; charset=utf-8', this.props.title + '.html');
+    if (key === 'markdown') {
+      exportFile(markdown, 'text/markdown; charset=utf-8', title + '.md');
+    } else if (key === 'html') {
+      const file = generateStyledHTML(data);
+
+      exportFile(file, 'text/plain; charset=utf-8', title + '.html');
     }
   };
 
-  render() {
-    return (
-      // {...this.props} 是为了让Menu组件正确地获取到作为overlay的样式属性,
-      // 这一点并没有在官方文档中有所说明，但在issue中找到这个解决方法
-      <Menu {...this.props} onClick={this.handleMenuItemClick}>
-        <Menu.Item key="markdown">
-          Export as Markdown
-        </Menu.Item>
-        <Menu.Item key="html">
-          Export as HTML
-        </Menu.Item>
-        <Menu.Item key="3">
-          Help
-        </Menu.Item>
-      </Menu>
-    );
-  }
-}
+  return (
+    // {...rest} 是为了让Menu组件正确地获取到作为overlay的样式属性,
+    // 这一点并没有在官方文档中有所说明，但在issue中找到这个解决方法
+    <Menu {...rest} onClick={handleMenuItemClick}>
+      <Menu.Item key="markdown">
+        Export as Markdown
+      </Menu.Item>
+      <Menu.Item key="html">
+        Export as HTML
+      </Menu.Item>
+      {/*<Menu.Item key="3">*/}
+        {/*Help*/}
+      {/*</Menu.Item>*/}
+    </Menu>
+  );
+};
 
-const MoreButtonInEditPage = ({ title, markdown, htmlOutput }) => {
+const MoreButtonInEditPage = (props) => {
   return (
     <Dropdown
       className={styles.button}
       overlay={
         <MoreMenuInEditPage
-          title={title}
-          markdown={markdown}
-          html={htmlOutput}
+          data={props}
         />
       }
       trigger={['click']}
