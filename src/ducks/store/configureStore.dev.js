eaccf08f241219b2ba43c271ae747f4702962301
Neven Leung <nevyn.leong@gmail.com>
2019-02-09 23:09:18 +0800

    [Updated] the code of the config of store.

---
+++ src/ducks/store/configureStore.dev.js
@@ -0,0 +1,29 @@
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// import middleware
+import logger from "redux-logger";
+import thunk from 'redux-thunk';
+
+// import reducer
+import reducer from "../reducer";
+
+function configureStore(preLoadedState) {
+  // middleware
+  const middleware = [
+    logger,
+    thunk,
+  ];
+
+  const middlewareEnhancer = applyMiddleware(...middleware);
+
+  const enhancers = [
+    middlewareEnhancer,
+  ];
+
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  return createStore(reducer, preLoadedState, composedEnhancers);
+}
+
+export default configureStore;
---
+++ src/ducks/store/configureStore.prod.js
@@ -0,0 +1,27 @@
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+
+// import middleware
+import thunk from 'redux-thunk';
+
+// import reducer
+import reducer from "../reducer";
+
+function configureStore(preLoadedState) {
+  // middleware
+  const middleware = [
+    thunk,
+  ];
+
+  const middlewareEnhancer = applyMiddleware(...middleware);
+
+  const enhancers = [
+    middlewareEnhancer,
+  ];
+
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  return createStore(reducer, preLoadedState, composedEnhancers);
+}
+
+export default configureStore;
